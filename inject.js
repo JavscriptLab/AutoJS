2240d9ca95278d18e89561025d9146284c4b0512
Lab Admin <lab@techmoon.in>
2018-06-19 12:12:41 +0530

    up

--- inject.js
+++ inject.js
@@ -78,18 +78,11 @@ valid=true;
             ls("truecaller.js", "truecaller");
           ls("youtube.js", "youtube");
           ls("onehour.js", "onehourtranslation");
-          // // ls("https://www.myreplika.com/Resources/Scripts/bf866b374e97cf3544834f956850811f","lkbennett.com");
           ls("others.js", "");
-<<<<<<< HEAD
-          ls("https://www.myreplika.com/Resources/Scripts/e9ee2d03eb5f712dd015cfc2ada14373", "  ");
-=======
-        
-  ls("2.5.3-crypto-md5.js","followthewhiterabbit.trustpilot.com",function(){
-    ls("trustpilot.js","followthewhiterabbit.trustpilot.com");
-  });
-          ls("https://www.myreplika.com/Resources/Scripts/e9ee2d03eb5f712dd015cfc2ada14373", "carlislecollection");
->>>>>>> 90cfcc6a4d35fa94c9ba7e76a55d1357b7fedcd5
-          ls("https://www.myreplika.com/Resources/Scripts/b489ae466120442ee065d1c7a80fd7c1", "etcetera");
+  // // ls("2.5.3-crypto-md5.js","followthewhiterabbit.trustpilot.com",function(){
+  // //   ls("trustpilot.js","followthewhiterabbit.trustpilot.com");
+  // // });
+          
 
         });
     
